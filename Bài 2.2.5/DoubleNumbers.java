206430029c81a216f0e60eaed93b45777d8b53da
Trần Cường <cuongtranvan609@gmail.com>
2026-03-24 16:41:18 +0700

    Add files via upload

---
+++ Bài 2.2.5/DoubleNumbers.java
@@ -0,0 +1,33 @@
+package LAB01_2403;
+
+import javax.swing.JOptionPane;
+
+public class DoubleNumbers {
+    public static void main(String[] args) {
+        
+        String strNum1 = JOptionPane.showInputDialog("Nhập số thứ nhất:");
+        String strNum2 = JOptionPane.showInputDialog("Nhập số thứ hai:");
+
+        double num1 = Double.parseDouble(strNum1);
+        double num2 = Double.parseDouble(strNum2);
+
+        // Tính toán
+        double tong = num1 + num2;
+        double hieu = num1 - num2;
+        double nhan = num1 * num2;
+
+        String message = "Tổng: " + tong + "\n"
+                       + "Hiệu: " + hieu + "\n"
+                       + "Nhân: " + nhan + "\n";
+
+        if (num2 != 0) {
+            double chia = num1 / num2;
+            message += "Chia: " + chia;
+        } else {
+            message += "Chia: Lỗi! Không thể chia cho 0.";
+        }
+
+        // Hiển thị kết quả bằng hộp thoại
+        JOptionPane.showMessageDialog(null, message);
+    }
+}
