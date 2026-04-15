60d8f32395d1a57271137b6d1567043e1ae640cc
Trần Cường <cuongtranvan609@gmail.com>
2026-04-16 01:07:23 +0700

    implement parameter passing test and correct swap method

---
+++ DigitalVideoDisc.java
@@ -0,0 +1,81 @@
+package LAB03;
+
+public class DigitalVideoDisc {
+    private String title;
+    private String category;
+    private String director;
+    private int length;
+    private float cost;
+
+    // 5. Classifier Member và Instance Member 
+    private int id; // Thuộc tính instance 
+    private static int nbDigitalVideoDiscs = 0; // Thuộc tính class (static) 
+    // Getter & Setter
+    public String getTitle() {
+        return title;
+    }
+
+    // Yêu cầu 3: Tạm thời thêm setter cho title để test truyền tham số 
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public String getDirector() {
+        return director;
+    }
+
+    public int getLength() {
+        return length;
+    }
+
+    public float getCost() {
+        return cost;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    // Constructors
+    // Mỗi khi một instance được tạo, nbDigitalVideoDiscs phải được cập nhật và gán cho id 
+    
+    public DigitalVideoDisc(String title) {
+        this.title = title;
+        nbDigitalVideoDiscs++;
+        this.id = nbDigitalVideoDiscs;
+    }
+
+    public DigitalVideoDisc(String category, String title, float cost) {
+        this.category = category;
+        this.title = title;
+        this.cost = cost;
+        nbDigitalVideoDiscs++;
+        this.id = nbDigitalVideoDiscs;
+    }
+
+    public DigitalVideoDisc(String title, String category, String director, int length, float cost) {
+        this.title = title;
+        this.category = category;
+        this.director = director;
+        this.length = length;
+        this.cost = cost;
+        nbDigitalVideoDiscs++;
+        this.id = nbDigitalVideoDiscs;
+    }
+
+    // 6. Phương thức bổ sung 
+    @Override
+    public String toString() {
+        return "DVD - " + title + " - " + category + " - " + director + " - " + length + ": " + cost + " $";
+    }
+
+    // Kiểm tra tiêu đề có khớp để phục vụ hàm search 
+    public boolean isMatch(String title) {
+        
+        return this.title.equalsIgnoreCase(title);
+    }
+}
